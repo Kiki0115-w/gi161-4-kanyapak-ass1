575ce78674e68bc42ffa2b290eb9380b107303d8
Kiki0115-w <kanyapak.wongt@bumail.net>
2024-09-14 19:27:51 +0700

    ass-1-

---
+++ Assets/Scripts/IronMan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+
+class IronMan
+{
+    //น.ส.1กัญญาภ้ค วงษ์ต่าย Section 229B เลขที่ 4
+
+
+
+    // Attributes
+    public string Name;
+    public int Hp;
+    public string SuitColor;
+    private float armorStrength;
+
+
+    // Contructor
+    public IronMan(string newName, int newHp, string newSuitColor)
+    {
+        Name = newName;
+        Hp = newHp;
+        SuitColor = newSuitColor;
+
+        armorStrength = 10;
+    }
+
+    public void Fly()
+    {
+        Debug.Log($"------ {Name} is flying");
+            
+    }
+
+    public void ShootLaser()
+    {
+        Debug.Log($"====== {Name} is shooting lasers");
+
+    }
+
+    public void UpdateAromorStrength( float strength)
+    {
+
+        armorStrength += strength; 
+        Debug.Log($"{Name} updated their armor to {armorStrength}");
+
+    }
+
+    public bool IsDead()
+    {
+        if (Hp <= 0)
+        {
+            return true;
+        }
+        return false;
+    }  
+
+
+    public void TakeDamage(int damage)
+    {
+        Hp -= damage;  // เลือดลดดาเมท
+        Debug.Log($"{Name}: took {damage} damage, Hp: {Hp}"); //บอกว่าโดนต่อยแล้วเลือดลด
+
+
+        if (IsDead())
+        {
+            Debug.Log($"{Name} is dead!!"); //ถ้าขิตให้รันขึ้น ชื่อ...ตายแล้ว
+        }
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    //น.ส.1กัญญาภ้ค วงษ์ต่าย Section 229B เลขที่ 4
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IronMan ironMan = new IronMan("Tony", 100, "Red");
+        Debug.Log($"IronMan name: {ironMan.Name}, Hp: {ironMan.Hp}, Color: {ironMan.SuitColor} ");
+
+        ironMan.Fly();
+        ironMan.ShootLaser();
+        ironMan.UpdateAromorStrength(5.25f);
+
+
+        CaptainAmerica captainAmerica = new CaptainAmerica("Stave", 100, "Blue");
+        Debug.Log($"CaptainAmerica name: {captainAmerica.Name}, Hp: {captainAmerica.Hp}, Color: {captainAmerica.SuitColor}");
+
+        captainAmerica.LeapAndJump();
+        captainAmerica.ThrowShield();
+        captainAmerica.UpdateAromorStrength(5.25f);
+
+
+
+        System.Random rand = new System.Random(); //ใช้ในการรัน
+        for (int i = 0; i < 5; i++)
+        {
+            // IronMan โจมตี
+            ironMan.ShootLaser();
+            int damageToCaptainAmerica = rand.Next(10, 21);
+            captainAmerica.TakeDamage(damageToCaptainAmerica);
+            //เช็คว่ากัปตันขิตยัง
+            if (captainAmerica.IsDead())
+            {
+                break;
+            }
+
+
+
+            // กัปตันต่อยไอรอนแมน
+            captainAmerica.ThrowShield();
+            int damageToironMan = rand.Next(10, 21);
+            ironMan.TakeDamage(damageToironMan);
+            //เช็คว่าไอรอนแมนขิตยัง
+            if (ironMan.IsDead())
+            {
+                break;
+            }
+
+
+        }
+
+        
+    }// ใส่ class Random ยังไงหรอคะ TT
+}
